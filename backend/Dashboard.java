264a281fd8d0bb12aeb2abe5d265003696990f8b
icanggrni <annisaanggrainiica@gmail.com>
2022-01-04 10:23:01 +0700

    Initial Project

---
+++ backend/Dashboard.java
@@ -0,0 +1,9 @@
+class Hello{
+	public static void main(String[] args){
+		System.out.println("Apsa Annisa : Aplikasi Java 2020");
+		System.out.println("--- *** ---");
+		System.out.printIn("END");
+	}
+}
+
+user 2
