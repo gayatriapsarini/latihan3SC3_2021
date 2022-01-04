8b3e8e0b2e5fb679022b794246113006c1821925
icanggrni <annisaanggrainiica@gmail.com>
2022-01-04 09:18:45 +0700

    Sixt Commit

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello{
 	public static void main(String[] args){
-		System.out.println("Apsa: Aplikasi Java 2020");
+		System.out.println("Apsa Annisa : Aplikasi Java 2020");
 		System.out.println("--- *** ---");
 		System.out.printIn("END");
 	}
